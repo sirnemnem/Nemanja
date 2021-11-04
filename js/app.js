fb4a15109a0c0df70fcc90944b5d544653751cd9
Vollbier1589 <83216654+Vollbier1589@users.noreply.github.com>
2021-11-04 17:31:59 +0100

    test

--- js/app.js
+++ js/app.js
@@ -55,6 +55,7 @@ const coffees = [
 document.addEventListener("DOMContentLoaded", showCoffees);
 */
 
+/*
 function getUserMedia(constraints) {
   // if Promise-based API is available, use it
   if (navigator.mediaDevices) {
@@ -101,6 +102,7 @@ function getStream (type) {
       alert('Error: ' + err);
     });
 }
+*/
 
 function getUserMedia(options, successCallback, failureCallback) {
   var api = navigator.getUserMedia || navigator.webkitGetUserMedia ||
@@ -111,8 +113,6 @@ function getUserMedia(options, successCallback, failureCallback) {
 }
 
 var theStream;
-var theRecorder;
-var recordedChunks = [];
 
 function getStream() {
   if (!navigator.getUserMedia && !navigator.webkitGetUserMedia &&
@@ -121,10 +121,12 @@ function getStream() {
     return;
   }
 
-  var constraints = {video: true, audio: true};
+  var constraints = {
+    video: true
+  };
+
   getUserMedia(constraints, function (stream) {
     var mediaControl = document.querySelector('video');
-
     if ('srcObject' in mediaControl) {
       mediaControl.srcObject = stream;
     } else if (navigator.mozGetUserMedia) {
@@ -132,21 +134,31 @@ function getStream() {
     } else {
       mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
     }
-
     theStream = stream;
-    try {
-      recorder = new MediaRecorder(stream, {mimeType : "video/webm"});
-    } catch (e) {
-      console.error('Exception while creating MediaRecorder: ' + e);
-      return;
-    }
-    theRecorder = recorder;
-    console.log('MediaRecorder created');
-    recorder.ondataavailable = recorderOnDataAvailable;
-    recorder.start(100);
   }, function (err) {
     alert('Error: ' + err);
   });
+}
+
+function takePhoto() {
+  if (!('ImageCapture' in window)) {
+    alert('ImageCapture is not available');
+    return;
+  }
+
+  if (!theStream) {
+    alert('Grab the video stream first!');
+    return;
+  }
+
+  var theImageCapturer = new ImageCapture(theStream.getVideoTracks()[0]);
+
+  theImageCapturer.takePhoto()
+    .then(blob => {
+      var theImageTag = document.getElementById("imageTag");
+      theImageTag.src = URL.createObjectURL(blob);
+    })
+    .catch(err => alert('Error: ' + err));
 }
 
 /*
@@ -175,77 +187,7 @@ function download() {
   }, 100);
 */
 
-function getUserMedia(options, successCallback, failureCallback) {
-  var api = navigator.getUserMedia || navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia || navigator.msGetUserMedia;
-  if (api) {
-    return api.bind(navigator)(options, successCallback, failureCallback);
-  }
-}
 
-var theStream;
-var theRecorder;
-var recordedChunks = [];
-
-function getStream() {
-  if (!navigator.getUserMedia && !navigator.webkitGetUserMedia &&
-    !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
-    alert('User Media API not supported.');
-    return;
-  }
-
-  var constraints = {video: true, audio: true};
-  getUserMedia(constraints, function (stream) {
-    var mediaControl = document.querySelector('video');
-
-    if ('srcObject' in mediaControl) {
-      mediaControl.srcObject = stream;
-    } else if (navigator.mozGetUserMedia) {
-      mediaControl.mozSrcObject = stream;
-    } else {
-      mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
-    }
-
-    theStream = stream;
-    try {
-      recorder = new MediaRecorder(stream, {mimeType : "video/webm"});
-    } catch (e) {
-      console.error('Exception while creating MediaRecorder: ' + e);
-      return;
-    }
-    theRecorder = recorder;
-    console.log('MediaRecorder created');
-    recorder.ondataavailable = recorderOnDataAvailable;
-    recorder.start(100);
-  }, function (err) {
-    alert('Error: ' + err);
-  });
-}
-
-function recorderOnDataAvailable(event) {
-  if (event.data.size == 0) return;
-  recordedChunks.push(event.data);
-}
-
-function download() {
-  console.log('Saving data');
-  theRecorder.stop();
-  theStream.getTracks()[0].stop();
-
-  var blob = new Blob(recordedChunks, {type: "video/webm"});
-  var url = (window.URL || window.webkitURL).createObjectURL(blob);
-  var a = document.createElement("a");
-  document.body.appendChild(a);
-  a.style = "display: none";
-  a.href = url;
-  a.download = 'test.webm';
-  a.click();
-
-  // setTimeout() here is needed for Firefox.
-  setTimeout(function () {
-      (window.URL || window.webkitURL).revokeObjectURL(url);
-  }, 100);
-}
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
