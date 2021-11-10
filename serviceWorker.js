9e51da78b6c29fa627e214db439fb8a3b51633a1
Edgar <83216654+Vollbier1589@users.noreply.github.com>
2021-11-10 21:56:17 +0100

    test install

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-const staticDevCoffee = "dev-coffee-site-v1";
+const staticEdgars Website = "dev-coffee-site-v1";
 const assets = [
   "/",
   "/index.html",
@@ -17,7 +17,7 @@ const assets = [
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
+    caches.open(staticEdgars Website).then(cache => {
       cache.addAll(assets);
     })
   );
