302516d07f5a7a5c29468544d6605db9c49270a5
Vollbier1589 <83216654+Vollbier1589@users.noreply.github.com>
2021-11-11 16:08:55 +0100

    location

--- js/app.js
+++ js/app.js
@@ -255,7 +255,26 @@ function download() {
   }, 100);
 */
 
+var target = document.getElementById('target');
+var watchId;
 
+function appendLocation(location, verb) {
+  verb = verb || 'updated';
+  var newLocation = document.createElement('p');
+  newLocation.innerHTML = 'Location ' + verb + ': ' + location.coords.latitude + ', ' + location.coords.longitude + '';
+  target.appendChild(newLocation);
+}
+
+if ('geolocation' in navigator) {
+  document.getElementById('askButton').addEventListener('click', function () {
+    navigator.geolocation.getCurrentPosition(function (location) {
+      appendLocation(location, 'fetched');
+    });
+    watchId = navigator.geolocation.watchPosition(appendLocation);
+  });
+} else {
+  target.innerText = 'Geolocation API not supported.';
+}
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
